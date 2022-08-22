3faa96899f3767f750800b591119dc1c73c21be6
BadgerSIO <saadibneomar@gmail.com>
2022-08-22 10:27:12 +0600

    testing player card js

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function addNametolist(id) {
+  const newListitem = document.createElement("li");
+  newListitem.innerHTML = "valuefromp";
+  document.getElementById(id).appendChild(newListitem);
+}
+addNametolist(["playerList"]);
