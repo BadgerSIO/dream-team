7b51dbc88e519a6edec7b1f36434242f222c25bb
BadgerSIO <saadibneomar@gmail.com>
2022-08-23 10:15:50 +0600

    updated

--- js/app.js
+++ js/app.js
@@ -65,17 +65,17 @@ document
     const managerCost = getNumbers("managerCost", false, true);
     const coachCost = getNumbers("coachCost", false, true);
     if (isNaN(expensesPlayerOnly) || isNaN(managerCost) || isNaN(coachCost)) {
-      if (isNaN(expensesPlayerOnly)) {
+      if (isNaN(expensesPlayerOnly) || expensesPlayerOnly == 0) {
         alert("Calculate the expense of players first");
-        document.getElementById("managerCost").value = "";
-        return;
-      } else if (isNaN(managerCost)) {
-        alert("invalid manager cost Input");
         document.getElementById("managerCost").value = "";
         return;
       } else if (isNaN(coachCost)) {
         alert("invalid coach cost Input");
         document.getElementById("coachCost").value = "";
+        return;
+      } else if (isNaN(managerCost)) {
+        alert("invalid manager cost Input");
+        document.getElementById("managerCost").value = "";
         return;
       }
       return;
